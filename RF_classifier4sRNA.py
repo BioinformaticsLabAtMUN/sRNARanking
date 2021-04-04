f5f5d1ac843ff026cec512dd3843bf094b3e2269
mnagari <megha@LAPTOP-JHJ4EHT4.localdomain>
2021-04-05 05:00:11 +0530

    added RF model implemented in python

---
+++ RF_classifier4sRNA.py
@@ -0,0 +1,22 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='Input data (feature table obtained by sRNACharP)',default=None,required=True,metavar='character')
+parser.add_argument('-o', '--out', help='Filename to output predictions [default= outPredictions.txt]',default="outPredictions.txt",metavar='character')
+args = parser.parse_args()
+
+import pickle
+random_forest_model = pickle.load(open("RF_classifier4sRNA.sav", 'rb'))
+
+import pandas
+
+data = pandas.read_table(args.input)
+probability_predictions = random_forest_model.predict_proba(data)
+output_data = {"No sRNA": probability_predictions[:, 0], "sRNA": probability_predictions[:, 1]}
+
+# Creates pandas DataFrame.
+output_data_table = pandas.DataFrame(output_data, index=data.index)
+output_data_table = output_data_table.round(4)
+output_data_table.to_csv(args.out,sep="\t")
+
+
